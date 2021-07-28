ae056326a9871e10373732afcc972be5b8d64be3
Indu <indurevathy@gmail.com>
2021-07-28 12:34:34 +0200

    03 Decklartion+Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -10,10 +10,21 @@
 
 //Deklaration + Wertzuweisung
 
-let firstName;//Deklaration
+/*let firstName;//Deklaration
 firstName = "Max";//wertzuweisung
 console.log(firstName);
 
 let familyName = "Mütze";//Deklaration und Werkzuweisung
 console.log(familyName);
-console.log(firstName + " " + familyName);
+console.log(firstName + " " + familyName);*/
+
+// 03 Deklaration + Wertzuweisung
+
+let firstName,familyName;
+firstName = prompt("Bitte Vorname eingeben:");
+familyName = prompt("Bitte Familyname eingeben:");
+console.log(firstName);
+console.log(familyName);
+console.log("Hallo " + firstName + " " + familyName +"!");
+
+
