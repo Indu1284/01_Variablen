70d8000ccc4d78817b3c2eaed98493386c03da10
Indu <indurevathy@gmail.com>
2021-07-28 12:12:31 +0200

    Deklaration + Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,19 @@
 /***** Variablen 01 *******/
+//Zeilen-kommentar
+/**
+ * 
+ * Block - kommentar
+ */
+// Ausgabe
+//console.log("Hallo Welt!");
+//console.log(firstName);
 
+//Deklaration + Wertzuweisung
+
+let firstName;//Deklaration
+firstName = "Max";//wertzuweisung
+console.log(firstName);
+
+let familyName = "Mütze";//Deklaration und Werkzuweisung
+console.log(familyName);
+console.log(firstName + " " + familyName);
